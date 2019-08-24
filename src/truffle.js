e3d93cfa63e0365b5b882457b737c8daa685384e
bneiluj <julien.bouteloup@gmail.com>
2019-08-25 01:25:26 +0200

    add src/truffle.js

---
+++ src/truffle.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  migrations_directory: "./migrations",
+  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+  networks: {
+    develop: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
