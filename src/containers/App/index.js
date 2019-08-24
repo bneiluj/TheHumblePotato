4908200097e8aeac14287a1b7507c7087d4d78a9
bneiluj <julien.bouteloup@gmail.com>
2019-08-24 23:27:35 +0200

    merge server info into dapp

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -4,36 +4,58 @@ import TwitterLogin from 'react-twitter-auth';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = { isAuthenticated: false, user: null, token: ''};
+  }
+
+  onSuccess = (response) => {
+    const token = response.headers.get('x-auth-token');
+    response.json().then(user => {
+      if (token) {
+        this.setState({isAuthenticated: true, user: user, token: token});
+      }
+    });
+  };
+
+  onFailed = (error) => {
+    alert(error);
+  };
+
+  logout = () => {
+    this.setState({isAuthenticated: false, token: '', user: null})
+  };
+
   render() {
+    let content = !!this.state.isAuthenticated ?
+      (
+        <div>
+          <p>Authenticated</p>
+          <div>
+            {this.state.user.email}
+          </div>
+          <div>
+            <button onClick={this.logout} className="button" >
+              Log out
+            </button>
+          </div>
+        </div>
+      ) :
+      (
+        <TwitterLogin loginUrl="http://localhost:4000/api/v1/auth/twitter"
+                      onFailure={this.onFailed} onSuccess={this.onSuccess}
+                      requestTokenUrl="http://localhost:4000/api/v1/auth/twitter/reverse"/>
+      );
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {content}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <TwitterLogin
-            loginUrl="https://api.twitter.com/oauth/"
-            onFailure={console.log}
-            onSuccess={console.log}
-            requestTokenUrl="http://127.0.0.1:8080/login"
-            credentials="include"
-            customHeaders={{
-              // "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Origin": "http://127.0.0.1:8080",
-              "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-              // "Access-Control-Expose-Headers": "X-My-Custom-Header",
-              'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS'
-            }}
-          />
         </header>
       </div>
     );
