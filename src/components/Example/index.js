f29c2e3359e400d85325316945363a3361fe01c1
Leopold Joy <leo@leopoldjoy.com>
2019-08-24 14:52:00 +0200

    setup React component/container directory structure

---
+++ src/components/Example/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Example.css';
+
+function Example() {
+  return (
+    <div className="Example">
+      example component
+    </div>
+  );
+}
+
+export default Example;
