e894c98829f48db09e03d60a5bb888eff706a252
Leopold Joy <leo@leopoldjoy.com>
2019-08-25 04:34:08 +0200

    setup TargetCard component for Profile container

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -3,6 +3,7 @@ import './Profile.css';
 import { drizzleConnect } from 'drizzle-react';
 // import { bindActionCreators } from "redux";
 import PropTypes from 'prop-types';
+import TargetCard from '../../components/TargetCard';
 // import * as 3boxActions from "../../actions/3box";
 import Box from '3box';
 
@@ -18,10 +19,17 @@ class Profile extends React.Component {
     console.log(profile);
   }
   render() {
+    let target = {
+      'name': 'Emin Gün Sirer',
+      'followers': '75,823',
+      'pot': '5,495',
+      'number': '18'
+    };
+
     return (
       <div className="Profile">
         <div>
-          <p>{"{INSERT Profile CONTENT HERE}"}</p>
+          <TargetCard target={target} />
         </div>
       </div>
     );
